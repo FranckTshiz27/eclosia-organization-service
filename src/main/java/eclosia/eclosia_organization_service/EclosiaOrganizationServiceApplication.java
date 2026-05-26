819fc45e4b6814457b40eae8687c785c319a15ca
François Kalombo <ftshizubu@rawsur.com>
2026-05-26 18:54:24 +0100

    first commit

---
+++ src/main/java/eclosia/eclosia_organization_service/EclosiaOrganizationServiceApplication.java
@@ -0,0 +1,13 @@
+package eclosia.eclosia_organization_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EclosiaOrganizationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EclosiaOrganizationServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/eclosia/eclosia_organization_service/EclosiaOrganizationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package eclosia.eclosia_organization_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EclosiaOrganizationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
